33769e3c6ce6ce58bcd34857275433bba91dfaaf
Moe Lwin Hein <hein.moelwin@gmail.com>
2019-10-08 20:51:35 +0800

    Added node_redis

--- app/server.js
+++ app/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const config = require('../config');
+const Redis = require('redis');
+
 const serverConfig = config.server;
 const redisConfig = config.redis;
 
+const redis = Redis.createClient(redisConfig.port, redisConfig.host);
+
 const app = express();
+
+app.get('/', async (req, res) => {
+	res.send('Welcome');
+});
+
+app.get('/keys', async (req, res) => {
+	redis.keys('*', (err, keys) => {
+		res.json(keys);
+	});
+});
 
 app.listen(serverConfig.port, serverConfig.host, () => {
 	console.log(`app running on http://${serverConfig.host}:${serverConfig.port}`);
